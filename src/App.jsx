10df1f6719235755cee28de736b729b27f0e3a0e
RaviGahire <ravigahire3@gmail.com>
2025-10-02 15:01:05 +0530

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ export class App extends React.Component {
         <div className="flex items-center flex-col md:flex-row md:justify-center flex-wrap bg-gradient-to-r from-blue-50 to-blue-100 min-h-screen p-4">
           <Employee employees={this.state.employees} />
         </div>
-    <Footer />
+        <Footer />
 
       </>
     )
